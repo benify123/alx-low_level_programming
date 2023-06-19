e1dfea3fe5d0300b371fd17117e8df56a3fe3db9
benify123 <okoroaforbenedictifeanyi@gmail.com>
2023-06-19 12:38:25 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * maini - Entry point
- *
- * Return: Always 0 (Success)
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	/*h*/
 	int n;
 
 	srand(time(0));
